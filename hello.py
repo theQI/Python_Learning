8dce5ab4c330b9cd23184a117d2db11d902f5cd3
Laxman Panthi <laxpanthi1216@gmail.com>
2017-02-17 09:00:44 -0500

    used jupyter notebooks

---
+++ hello.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 11 15:00:24 2017
+@author: laxpa
+"""
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None"""
+    print("Hello World")
+
+# main program starts here
+hello()
---
+++ while-to.py
@@ -0,0 +1,5 @@
+sum = 0
+a = input("enter a number")
+while a < 0
+    sum = sum + a
+    print(sum)
